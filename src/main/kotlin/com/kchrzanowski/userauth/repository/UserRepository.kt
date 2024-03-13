72e3c64840b31a6cff40b9d9f18c37772bd2a97d
Konrad <chrzanekk@gmail.com>
2024-03-13 19:18:30 +0100

    rearange repository to encode password in in-memory db

--- src/main/kotlin/com/kchrzanowski/userauth/repository/UserRepository.kt
+++ src/main/kotlin/com/kchrzanowski/userauth/repository/UserRepository.kt
@@ -2,21 +2,27 @@ package com.kchrzanowski.userauth.repository
 
 import com.kchrzanowski.userauth.enumerated.Role
 import com.kchrzanowski.userauth.model.User
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Repository
 import java.util.*
 
 //todo again extend to connect to real DB
 
 @Repository
-class UserRepository {
+class UserRepository(
+    private val encoder: PasswordEncoder
+) {
 
     private val users = mutableListOf(
-            User(id = UUID.randomUUID(), "email1@1emai.com", "pass1", Role.USER),
-            User(id = UUID.randomUUID(), "email2@1emai.com", "pass2", Role.ADMIN),
-            User(id = UUID.randomUUID(), "email3@1emai.com", "pass3", Role.USER),
+            User(id = UUID.randomUUID(), "email1@1emai.com", encoder.encode("pass1"), Role.USER),
+            User(id = UUID.randomUUID(), "email2@1emai.com", encoder.encode("pass2"), Role.ADMIN),
+            User(id = UUID.randomUUID(), "email3@1emai.com", encoder.encode("pass3"), Role.USER),
     )
 
-    fun save(user: User): Boolean = users.add(user)
+    fun save(user: User): Boolean {
+        val updated = user.copy(password = encoder.encode(user.password))
+        return users.add(updated)
+    }
 
     fun findByEmail(email: String): User? =
             users.firstOrNull { it.email == email }
