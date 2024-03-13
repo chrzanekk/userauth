a2d46a62108f54553a727700ac27fa3beb42e235
Konrad <chrzanekk@gmail.com>
2024-03-13 18:01:19 +0100

    added dependencies for spring security and JWT

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+	implementation("io.jsonwebtoken:jjwt-impl:0.12.5")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
+	implementation("org.springframework.boot:spring-boot-starter-security:3.2.3")
+
+	testImplementation("org.springframework.security:spring-security-test:6.2.2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
